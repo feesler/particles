33c701423703b4aeda2fe3c437684176c5b85674
Ilya Makarov <feesler@gmail.com>
2021-05-18 19:00:23 +0300

    * Draw massive stars with blue color

--- src/Star.js
+++ src/Star.js
@@ -3,6 +3,10 @@ import { Particle } from './Particle.js';
 export class Star extends Particle {
     constructor(x, y, mass = 500000) {
         super(x, y, 0, mass);
-        this.color = { r: 0xFF, g: 0xFF, b: 0x88 };
+        if (mass >= 1000000000) {
+            this.color = { r: 0x4D, g: 0xBE, b: 0xFF };
+        } else {
+            this.color = { r: 0xEE, g: 0xB0, b: 0x2A };
+        }
     }
 }
