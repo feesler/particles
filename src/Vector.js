9868f6ea5f64b1bf6f397b97bbfcdda7ac512213
Ilya Makarov <feesler@gmail.com>
2021-05-20 18:10:07 +0300

    + Add some methods for Vector class

--- src/Vector.js
+++ src/Vector.js
@@ -4,4 +4,39 @@ export class Vector {
         this.y = y;
         this.z = z;
     }
+
+    copy() {
+        return new Vector(this.x, this.y, this.z);
+    }
+
+    product(vector) {
+        return this.x * vector.x + this.y * vector.y + this.z * vector.z;
+    }
+
+    add(vector) {
+        this.x += vector.x;
+        this.y += vector.y;
+        this.z += vector.z;
+    }
+
+    substract(vector) {
+        this.x -= vector.x;
+        this.y -= vector.y;
+        this.z -= vector.z;
+    }
+
+    rotateAroundX(angle) {
+        this.y = this.y * Math.cos(angle) - this.z * Math.sin(angle);
+        this.z = this.y * Math.sin(angle) + this.z * Math.cos(angle);
+    }
+
+    rotateAroundY(angle) {
+        this.x = this.x * Math.cos(angle) + this.z * Math.sin(angle);
+        this.z = -this.x * Math.sin(angle) + this.z * Math.cos(angle);
+    }
+
+    rotateAroundZ(angle) {
+        this.x = this.x * Math.cos(angle) - this.y * Math.sin(angle);
+        this.y = this.x * Math.sin(angle) + this.y * Math.cos(angle);
+    }
 }
