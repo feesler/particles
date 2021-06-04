75d27f104c7a0ffe0cc7692edcc42160995d36f8
Ilya Makarov <feesler@gmail.com>
2021-06-04 15:28:10 +0300

    + Setup webpack build and development server

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+    mode: 'production',
+    target: ['web'],
+    entry: './src/index.js',
+    output: {
+        filename: 'index.js',
+        path: resolve(__dirname, 'dist'),
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+    ],
+    optimization: {
+        minimize: true,
+    },
+};
