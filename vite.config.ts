c425d77fd9684c7a2fe6bff5f10344474cd22b8b
Ilya Makarov <feesler@gmail.com>
2025-03-30 18:52:11 +0300

    + Added TypeScript and Vite alias for 'src' directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import path from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
     base: '/particles',
     plugins: [react(), svgr({ include: '**/*.svg' })],
+    resolve: {
+        alias: {
+            "src": path.resolve(__dirname, "./src"),
+        },
+    },
     css: {
         preprocessorOptions: {
             scss: {
